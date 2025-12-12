b779b12e8e5d1a3558127d5e2f58386144258942
BalakeKarbon <blakekarbon@gmail.com>
2025-12-12 23:42:36 +0000

    Removed unused file.

--- LogParseError.java
+++
@@ -1,6 +0,0 @@
-class LogParseError extends Exception {
-	private String line; // This might change as there could be an inter-line error probably?
-	public LogParseError(String message) {
-		super(message);
-	}
-}
